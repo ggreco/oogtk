5a2e0723071952c01b3ff6b51def00cb3c048970
ggreco <ggreco@47a0e186-da48-0410-a149-f5e7934c5b89>
2009-02-11 16:12:13 +0000

    - Improved documentation. - New gtk::Application-less example. - Sparse fixes and new methods.

---
+++ testnoapp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "oogtk.h"
+
+// this simple program test the use of OOGtk without the Application class
+// helper.
+
+static void clicked()
+{
+    std::cerr << "You clicked me!\n";
+}
+
+int main(int argc, char *argv[]) {
+    // initialize GTK, you can provide argc / argv or ignore them 
+    gtk::Application::Init(argc, argv);
+
+    // create some objects
+    gtk::Window win("Hello world window");
+    gtk::Button button("Click me!");
+    win.Border(16);
+    win.Child(button);
+
+    // attach a callback to them (to an external C function)
+    win.OnDelete((GCallback)gtk::Application::Quit);
+    // attach a callback to a local function
+    button.OnClick((GCallback)clicked);
+
+    // show the objects and run the app!
+    win.ShowAll();
+
+    gtk::Application::Run();
+
+    exit(0);
+}
