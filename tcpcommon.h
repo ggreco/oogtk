c446f0c9b383cdb3113c90fae1f3669c61e3c17e
ggreco <ggreco@47a0e186-da48-0410-a149-f5e7934c5b89>
2009-04-23 15:31:13 +0000

    - Methods and documentation for Pixmap, Window, TreeView. - Implemented socket handling and added an apropriate example.

--- tcpcommon.h
+++ tcpcommon.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 #define ERRNO errno
 #define closesocket(x) close(x)
 #define SOCKET_ERROR -1
@@ -13,17 +14,43 @@
 #include <netdb.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+typedef size_t len_t;
+
+#define initialize_tcpip() 
+#define msleep(x) usleep(x*1000)
+
 #else
 #include <winsock2.h>
+#include <sys/time.h>
+#define EINPROGRESS WSAEWOULDBLOCK
 #define EWOULDBLOCK WSAEWOULDBLOCK
 #define ECONNABORTED WSAECONNABORTED
+#define EALREADY WSAEALREADY
+#define EISCONN WSAEISCONN
 #define O_NONBLOCK 1234657
 #define ECONNRESET   WSAECONNRESET
 #define MSG_NOSIGNAL 0
 #define ERRNO WSAGetLastError()
+#define ioctl(x,y,z) ioctlsocket(x,y,(u_long*)(z))
 #define STRERROR tor_socket_strerror
+#define msleep(x) Sleep(x)
+
+#define initialize_tcpip() do {  WSADATA wsaData; \
+        if (WSAStartup(MAKEWORD( 2, 0 ), &wsaData) != 0) { \
+            OutputDebugString("Impossibile inizializzare winsock!"); \
+            exit(0); \
+        } \
+        } while (0)
+
+
+typedef int len_t;
 typedef unsigned long in_addr_t;
 const char *tor_socket_strerror(int);
+#define inet_ntop(a, ip, ipstr, len) do { strncpy(ipstr, inet_ntoa(*ip), len); } while(0)
+#ifdef NEED_GETTIMEOFDAY
+extern "C" int gettimeofday(struct timeval *, void *);
+#endif
+#define localtime_r(x,y) localtime(x)
 #endif
 
 #endif
